d5401634d972c28aedc6e3339d5c255b04c36a24
mdelage <mdelage@student.42.fr>
2015-03-05 17:17:20 +0100

    reindent default layout

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -54,71 +54,71 @@
   </head>
 
   <body>
-    <!-- To make sticky footer need to wrap in a div -->
+  <!-- To make sticky footer need to wrap in a div -->
     <div id="wrap">
-    <!-- Navbar -->
-    <div class="navbar navbar-default navbar-inverse navbar-fixed-top">
-       <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse navbar-ex1-collapse">
-                    <ul class="nav navbar-nav">
-            <li {{ (Request::is('/') ? ' class="active"' : '') }}><a href="{{{ URL::to('') }}}">Home</a></li>
-          </ul>
+      <!-- Navbar -->
+      <div class="navbar navbar-default navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+              <li {{ (Request::is('/') ? ' class="active"' : '') }}><a href="{{{ URL::to('') }}}">Home</a></li>
+            </ul>
 
-                    <ul class="nav navbar-nav pull-right">
-                        @if (Auth::check())
-                        @if (Auth::user()->hasRole('admin'))
-                        <li><a href="{{{ URL::to('admin') }}}">Admin Panel</a></li>
-                        @endif
-                        <li><a href="{{{ URL::to('user') }}}">Logged in as {{{ Auth::user()->username }}}</a></li>
-                        <li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
-                        @else
-                        <li {{ (Request::is('user/login') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/login') }}}">Login</a></li>
-                        <li {{ (Request::is('user/create') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/create') }}}">{{{ Lang::get('site.sign_up') }}}</a></li>
-                        @endif
-                    </ul>
-          <!-- ./ nav-collapse -->
+            <ul class="nav navbar-nav pull-right">
+              @if (Auth::check())
+                @if (Auth::user()->hasRole('admin'))
+                   <li><a href="{{{ URL::to('admin') }}}">Admin Panel</a></li>
+                @endif
+                <li><a href="{{{ URL::to('user') }}}">Logged in as {{{ Auth::user()->username }}}</a></li>
+                <li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
+              @else
+                <li {{ (Request::is('user/login') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/login') }}}">Login</a></li>
+                <li {{ (Request::is('user/create') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/create') }}}">{{{ Lang::get('site.sign_up') }}}</a></li>
+              @endif
+            </ul>
+            <!-- ./ nav-collapse -->
+          </div>
         </div>
       </div>
-    </div>
-    <!-- ./ navbar -->
+      <!-- ./ navbar -->
 
     <!-- Container -->
     <div class="container">
-      <!-- Notifications -->
-      @include('notifications')
-      <!-- ./ notifications -->
+    <!-- Notifications -->
+    @include('notifications')
+    <!-- ./ notifications -->
 
-      <!-- Content -->
-      @yield('content')
-      <!-- ./ content -->
+    <!-- Content -->
+    @yield('content')
+    <!-- ./ content -->
+  </div>
+  <!-- ./ container -->
+
+  <!-- the following div is needed to make a sticky footer -->
+  <div id="push"></div>
+  </div>
+  <!-- ./wrap -->
+
+
+  <div id="footer">
+    <div class="container">
+    <!--          <p class="muted credit">Laravel 4 Starter Site on <a href="https://github.com/andrew13/Laravel-4-Bootstrap-Starter-Site">Github</a>.</p>-->
     </div>
-    <!-- ./ container -->
-
-    <!-- the following div is needed to make a sticky footer -->
-    <div id="push"></div>
-    </div>
-    <!-- ./wrap -->
-
-
-      <div id="footer">
-        <div class="container">
-<!--          <p class="muted credit">Laravel 4 Starter Site on <a href="https://github.com/andrew13/Laravel-4-Bootstrap-Starter-Site">Github</a>.</p>-->
-        </div>
-      </div>
+  </div>
 
     <!-- Javascripts
     ================================================== -->
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
 
-        @yield('scripts')
+  @yield('scripts')
   </body>
 </html>
